0504e16a984b6b0c34e39bea7a43db0cbd685275
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-03-08 12:24:37 +0800

    Add vue-router functionality

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+ name: 'App',
+ components: {
+     HelloWorld,
+ },
+}
+</script>
+
+<style>
+
+</style>
