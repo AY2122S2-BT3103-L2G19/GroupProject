b868d3224c100b2a55dfd03442983a83eaeb37bf
karintang <77315323+karintang@users.noreply.github.com>
2022-03-09 08:58:04 +0800

    creating goals table

---
+++ src/components/GoalsTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <table id="SpendingGoals">
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Profession</th>
+    </tr>
+</table>
+
+</template>
+
+<script>
+export default {
+    name: 'GoalsTable'
+}
+</script>
+
+<style>
+
+</style>
