6ef0edc45cc40faecb6c0b1d2f3de96d53f13668
karintang <77315323+karintang@users.noreply.github.com>
2022-03-09 09:13:27 +0800

    Changes to Goals Table

--- src/components/GoalsTable.vue
+++ src/components/GoalsTable.vue
@@ -2,20 +2,20 @@
   <table id="SpendingGoals">
     <tr>
       <th>Category</th>
-      <th>Spent</th>
-      <th>Goal (Monthly)</th>
-      <th>Percentage of completion</th>
-      <th>Remaining Budget for the month</th>
+      <th>Spent ($)</th>
+      <th>Goal (Monthly) ($)</th>
+      <th>Percentage of completion (%)</th>
+      <th>Remaining Budget for the month ($)</th>
     </tr>
   </table>
 
   <table id="SpendingGoalsData">
     <tr>
-      <td>Category</td>
-      <td>Spent</td>
-      <td>Goal (Monthly)</td>
-      <td>Percentage of completion</td>
-      <td>Remaining Budget for the month</td>
+      <td>Food</td>
+      <td>5</td>
+      <td>2000</td>
+      <td>0.0025</td>
+      <td>1995</td>
     </tr>
   </table>
 </template>
