37851982db82b73bdeb32ae3148a143e525ca9f7
karintang <77315323+karintang@users.noreply.github.com>
2022-03-09 09:03:25 +0800

    Adding Goals table to Goals Page

--- src/views/Goals.vue
+++ src/views/Goals.vue
@@ -1,10 +1,12 @@
 <template>
     <h1>Hello from Goals</h1>
+    <goals-table />
 </template>
 
 <script>
+import GoalsTable from '../components/GoalsTable.vue'
 export default {
-
+  components: { GoalsTable },
 }
 </script>
 
