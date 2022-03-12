fe4fc40cc9d715ab659773425a45b9710181228b
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-03-12 14:35:37 +0800

    Test for CI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Hello from Home</h1>
-    <h2>Deployed from local test</h2>
+    <h2>Deployed from PR test</h2>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
