619e29ef9dda9bc480bf41ad2124b8c6e2ca100c
etbf <ernest-tan123@hotmail.com>
2022-03-12 18:24:47 +0800

    expenses
    done up skeleton of expenses page and expenses display page

--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -1,11 +1,15 @@
 <template>
     <h1>Hello from Expenses</h1>
+    <ExpensesDisplay/>
 </template>
 
 <script>
+import ExpensesDisplay from '../components/ExpensesDisplay.vue'
 export default {
-
+  name: "Expenses",
+  components: { ExpensesDisplay },
 }
+
 </script>
 
 <style>
