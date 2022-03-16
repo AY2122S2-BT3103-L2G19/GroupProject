db2e3b80291240d4323d6282cb19c1ae5c6ca36d
karintang <77315323+karintang@users.noreply.github.com>
2022-03-16 21:38:21 +0800

    adding spending goals button

--- src/views/Goals.vue
+++ src/views/Goals.vue
@@ -2,6 +2,7 @@
   <h1>Hello from Goals</h1>
   <h2>Spending Goals</h2>
   <button>Add Spending Goals</button>
+  <navbar />
   <SpendingGoals />
   <h2>Saving Goals</h2>
   <button>Add Saving Goals</button>
@@ -10,28 +11,29 @@
 
 <script>
 import SpendingGoals from "../components/SpendingGoals.vue";
-import {getAuth, onAuthStateChanged} from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Navbar from "../components/Navbar.vue";
 
 export default {
   name: "Goals",
-  components: { SpendingGoals },
+  components: { SpendingGoals, Navbar },
   data() {
     return {
-        user:false,
-    }
-},
+      user: false,
+    };
+  },
 
-mounted() {
+  mounted() {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
-        if (user) {
-            this.user = user;
-            console.log(user.email)
-        } else {
-          this.$router.push("/login")
-        }
-    })
-},
+      if (user) {
+        this.user = user;
+        console.log(user.email);
+      } else {
+        this.$router.push("/login");
+      }
+    });
+  },
 };
 </script>
 
