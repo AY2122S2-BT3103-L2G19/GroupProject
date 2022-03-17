9cbb397817744875b73dc67bfabe5535531bde2c
karintang <77315323+karintang@users.noreply.github.com>
2022-03-17 16:22:06 +0800

    adding uifire

--- src/uifire.js
+++ src/uifire.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import firebase from 'firebase/compat/app'
 
 // Your web app's Firebase configuration
@@ -15,3 +16,23 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export default firebase; 
+=======
+import firebase from "firebase/compat/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0qWFX7tBLdRSowKmLbmM50uIZesLcUc4",
+  authDomain: "group-project-65d9c.firebaseapp.com",
+  databaseURL:
+    "https://group-project-65d9c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "group-project-65d9c",
+  storageBucket: "group-project-65d9c.appspot.com",
+  messagingSenderId: "841085668550",
+  appId: "1:841085668550:web:a155ea49f291a9806a1bc7",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+>>>>>>> Stashed changes
