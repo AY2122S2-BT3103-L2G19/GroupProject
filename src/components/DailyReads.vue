1b3fc28bcf7d4f8017eb9b6f8be46dc2c21cfc6d
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-03-21 15:26:47 +0800

    Update DailyReads.vue

--- src/components/DailyReads.vue
+++ src/components/DailyReads.vue
@@ -2,20 +2,27 @@
   <div class="article">
     <h1 class="title">{{currentDateText()}}</h1>
     <h2 class="content"> 
-      <iframe src="https://dollarsandsense.sg/4-reasons-financial-literacy-important-young-singaporeans/" width="870" height="160" style="border: none;" name="myFrame"></iframe>
-      <p><a href="https://dollarsandsense.sg/4-reasons-financial-literacy-important-young-singaporeans/" target="myFrame">Open Article!</a></p>
+      <iframe id="iframe" v-bind:src="data" width="870" height="160" style="border: none;" name="myFrame"></iframe>
+      <a v-bind:href="data" target="_blank">Open Article!</a>
     </h2>
   </div>
 </template>
 
 <script>
+
+
 export default {
+  data() {
+    return {
+      data:"https://dollarsandsense.sg/4-reasons-financial-literacy-important-young-singaporeans/"
+    }
+  },
   methods: {
     currentDateText() {
       const current = new Date();
       const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
       return String(date) + ": Daily Article"
-    }
+    },
   }
 }
 </script>
