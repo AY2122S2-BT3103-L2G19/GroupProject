8d4987c64aeff74a4dd5049f67a70a9464ca9332
karintang <77315323+karintang@users.noreply.github.com>
2022-03-23 21:21:06 +0800

    firebase

--- src/components/SpendingGoals2.vue
+++ src/components/SpendingGoals2.vue
@@ -18,90 +18,31 @@
 import { getFirestore } from "firebase/firestore";
 import firebaseApp from "../firebase.js";
 const db = getFirestore(firebaseApp);
-// import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 
 export default {
   name: "SpendingGoals2",
   components: {},
 
+  data() {
+    return {
+      fbuser: "",
+      count: "",
+    };
+  },
+
   mounted() {
-    this.display("user1")
-    // const auth = getAuth();
+    const auth = getAuth();
+    console.log(auth, " fbuser from goals display before");
     // this.fbuser = auth.currentUser.email;
-    // console.log(this.fbuser, " this.fbuser")
-
-    // async function display(user) {
-    //   user = "user1";
-    //   let doc = await getDocs(
-    //     collection(db, String(user), "Spending Goals", "Goals")
-    //   );
-    //   let ind = 1;
-    //   doc.forEach((docs) => {
-    //     let docData = docs.data();
-    //     var table = document.getElementById("spendingGoalsTable");
-    //     var row = table.insertRow(ind);
-    //     var Category = docData.Category;
-    //     var goal = docData.Goals;
-
-    //     var cell1 = row.insertCell(0);
-    //     var cell2 = row.insertCell(1);
-    //     var cell3 = row.insertCell(2);
-    //     var cell4 = row.insertCell(3);
-    //     var cell5 = row.insertCell(4);
-    //     var cell6 = row.insertCell(5);
-    //     var cell7 = row.insertCell(6);
-    //     var cell8 = row.insertCell(7);
-    //     var cell9 = row.insertCell(8);
-
-    //     cell1.innerHTML = ind;
-    //     cell2.innerHTML = Category;
-    //     cell3.innerHTML = 0;
-    //     cell4.innerHTML = goal;
-    //     cell5.innerHTML = 0;
-    //     cell6.innerHTML = 0;
-    //     cell7.innerHTML = 0;
-    //     cell8.innerHTML = 0;
-
-    //     var editBut = document.createElement("button");
-
-    //     editBut.className = "Ebwt";
-    //     editBut.id = "editbutton";
-    //     editBut.innerHTML = "Edit";
-    //     editBut.onclick = function () {};
-    //     cell8.appendChild(editBut);
-
-    //     var delBut = document.createElement("button");
-    //     delBut.className = "bwt";
-    //     delBut.id = String(Category);
-    //     delBut.innerHTML = "Delete";
-    //     delBut.onclick = function () {
-    //       deleteInstrument(Category, String("user1"));
-    //     };
-    //     cell9.appendChild(delBut);
-    //     ind += 1;
-    //   });
-    // }
-
-    // display("user1");
-
-    // async function deleteInstrument(category, user) {
-    //   alert("You are going to delete " + category);
-    //   await deleteDoc(
-    //     doc(db, String(user), "Spending Goals", "Goals", category)
-    //   );
-    //   console.log("Document successfully deleted!");
-    //   let tb = document.getElementById("spendingGoalsTable");
-    //   while (tb.rows.length > 1) {
-    //     tb.deleteRow(1);
-    //   }
-    //   display(user);
-    // }
+    console.log(this.fbuser, " fbuser from goals display");
+    this.display(this.fbuser);
   },
 
   methods: {
-     async display(user) {
-      user = "user1";
+    async display(user) {
+      console.log(this.fbuser, " fbuser from goals");
       let doc = await getDocs(
         collection(db, String(user), "Spending Goals", "Goals")
       );
@@ -144,8 +85,8 @@ export default {
         delBut.className = "bwt";
         delBut.id = String(Category);
         delBut.innerHTML = "Delete";
-        delBut.onclick = function () {
-          deleteInstrument(Category, String("user1"));
+        delBut.onclick = () => {
+          this.deleteInstrument(Category, user);
         };
         cell9.appendChild(delBut);
         ind += 1;
@@ -157,13 +98,13 @@ export default {
       await deleteDoc(
         doc(db, String(user), "Spending Goals", "Goals", category)
       );
-      console.log("Document successfully deleted!");
       let tb = document.getElementById("spendingGoalsTable");
+      //delete everything, make data empty and call the display again
       while (tb.rows.length > 1) {
         tb.deleteRow(1);
       }
-    }
-
+      this.display(user);
+    },
   },
 };
 </script>
