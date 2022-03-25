00f11fd3b6129cdd9e9038ff1d59ac9e499b053e
karintang <77315323+karintang@users.noreply.github.com>
2022-03-25 20:59:37 +0800

    Edit button

---
+++ src/components/NavbarEditSpendingGoals.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <button class="button" @click="openModel">Edit</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarEditSpendingGoals",
+  methods: {
+    openModel() {
+      this.$emit("model-show2");
+    },
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+  height: 60px;
+  background: #fff;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.button {
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background: #84bddf;
+  color: #fff;
+  padding: 10px 15px;
+  cursor: pointer;
+  outline: none;
+}
+</style>
