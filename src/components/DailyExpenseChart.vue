fe118bc3a92ce5d9b701db162fd980cf8bf7b167
cf6600 <80244439+cf6600@users.noreply.github.com>
2022-03-25 17:39:39 +0800

    Daily Expense Line chart now takes data from user
    Linechart styling still needs fixing

--- src/components/DailyExpenseChart.vue
+++ src/components/DailyExpenseChart.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="chart">
-    <h4 class="title">wtf</h4>
-    <line-chart class = "user" width = 300px :data = "lineChartData"></line-chart>
+    <h4 class="title">Line Chart of expenses</h4>
+    <line-chart class = "user" width = 100% :data = "chartParams"></line-chart>
   </div>
 </template>
 
 <script>
+import firebaseApp from "../firebase.js";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+const db = getFirestore(firebaseApp);
 export default {
   name: 'DailyExpenseChart',
   data () {
     return {
-        lineChartData: {'Monday': 2, 'Tuesday': 5, 'Wednesday': 2,'Thursday': 5,'Friday': 6}
+        chartParams:[]
     }
+  },
+  methods: {
+    // const auth = getAuth();
+    // this.fbuser = auth.currentUser.email;
+    async display(currUser) {
+      console.log("Displaying user Expense Chart");
+      //currUser = "meow@poop.com";
+      console.log(currUser, " user");
+      let docs = await getDocs(
+        collection(db, String(currUser), "Transactions", "Expenses")
+      );
+
+      var trans = [];
+
+      docs.forEach((doc) => {
+        let docData = doc.data();
+        console.log(docData, " doc data");
+        let transDetails = [];
+        transDetails.push(docData.Date);
+        transDetails.push(docData.Amount);
+        trans.push(transDetails);
+      });
+
+      
+      this.chartParams = trans;
+      console.log(this.chartParams, " updated chart")
+
+      return trans;
+    },
+  },
+  mounted() {
+    this.display("meow@poop.com");
   }
 }
 </script>
