a2947ae88178dd3ea83ccb7e04f51ef93525daa9
karintang <77315323+karintang@users.noreply.github.com>
2022-03-28 19:59:32 +0800

    Spent column

--- src/components/SpendingGoals2.vue
+++ src/components/SpendingGoals2.vue
@@ -24,13 +24,12 @@ import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 
 export default {
   name: "SpendingGoals2",
-  components: { },
+  components: {},
 
   data() {
     return {
       fbuser: "",
-      modelStatus: false, 
-
+      modelStatus: false,
     };
   },
 
@@ -74,15 +73,17 @@ export default {
         cell7.innerHTML = 0;
         cell8.innerHTML = 0;
 
+        this.getExpense(user)
+
         var editBut = document.createElement("button");
 
         editBut.className = "Ebwt";
         editBut.id = "editbutton";
         editBut.innerHTML = "Edit";
         editBut.onclick = () => {
-          this.$emit("model-show2")
-          this.$emit("added")
-          console.log("edit on click ")
+          this.$emit("model-show2");
+          this.$emit("added");
+          console.log("edit on click ");
         };
         cell8.appendChild(editBut);
 
@@ -115,6 +116,86 @@ export default {
       this.modelStatus = !this.modelStatus;
     },
 
+    async getExpense(currUser) {
+      var expenses = [];
+      var expenseDocs = null;
+      expenseDocs = await getDocs(
+        collection(db, currUser, "Transactions", "Expenses")
+      );
+      //extract expenses
+      expenseDocs.forEach((doc) => {
+        let docData = doc.data();
+        let transDetails = [];
+        transDetails.push(docData.Date);
+        transDetails.push(docData.Amount);
+        expenses.push(transDetails);
+      });
+
+      console.log(expenses, " expenses")
+
+      var expensesByMonth = new Map();
+      for (let i = 0; i < expenses.length; i++) {
+        let month = expenses[i][0].slice(3, 5);
+        if (expensesByMonth.has(month)) {
+          let currAmt = expensesByMonth.get(month);
+          expensesByMonth.set(month, currAmt + expenses[i][1]);
+        } else {
+          expensesByMonth.set(month, expenses[i][1]);
+        }
+      }
+      console.log(expensesByMonth, "expense map check");
+      var expensesByMonthFinal = [];
+      //convert map back to array
+      expensesByMonth.forEach((value, key) => {
+        expensesByMonthFinal.push([key, value]);
+      });
+
+      var expensesByMonthSorted = expensesByMonthFinal.sort();
+
+      for (let i = 0; i < expensesByMonthSorted.length; i++) {
+        let monthNum = expensesByMonthSorted[i][0];
+        let month = "";
+        switch (monthNum) {
+          case "01":
+            month = "Jan";
+            break;
+          case "02":
+            month = "Feb";
+            break;
+          case "03":
+            month = "Mar";
+            break;
+          case "04":
+            month = "Apr";
+            break;
+          case "05":
+            month = "May";
+            break;
+          case "06":
+            month = "Jun";
+            break;
+          case "07":
+            month = "Jul";
+            break;
+          case "08":
+            month = "Aug";
+            break;
+          case "09":
+            month = "Sep";
+            break;
+          case "10":
+            month = "Oct";
+            break;
+          case "11":
+            month = "Nov";
+            break;
+          case "12":
+            month = "Dec";
+            break;
+        }
+        expensesByMonthSorted[i][0] = month;
+      }
+    },
   },
 };
 </script>
