388cb69ce7d0771b0f05e9d3d603129a5c10a29e
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-04-04 17:10:53 +0800

    Add Vuestic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router/index.js'
 import VueChartkick from 'vue-chartkick'
 import 'chartkick/chart.js'
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 
 // I dont know why i cant chain in using use(VueChartKick) before using router, online say can 
-createApp(App).use(router).use(VueChartkick).mount('#app')
+createApp(App).use(router).use(VueChartkick).use(VuesticPlugin).mount('#app')
