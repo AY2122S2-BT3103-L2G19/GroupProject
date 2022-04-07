ea53881308a07e17976db6f8397e1add1d0a35e7
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-04-07 17:34:09 +0800

    add resolve button

--- src/components/OwedPaymentsTable.vue
+++ src/components/OwedPaymentsTable.vue
@@ -8,6 +8,7 @@
         <th>Name</th>
         <th>Amount</th>
         <th>Date</th>
+        <th>Resolve</th>
         </tr>
     </table>
   </div>
@@ -15,7 +16,7 @@
 
 <script>
 import firebaseApp from "../firebase.js";
-import { getFirestore } from "firebase/firestore";
+import { setDoc, doc ,deleteDoc, getFirestore } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 
@@ -55,23 +56,55 @@ export default {
           var table = document.getElementById("table")
           var row = table.insertRow(start_sn)
 
-          var title = (data_required.title)
-          var amount = (data_required.amount)
-          var date = (data_required.date)
+          let title = (data_required.title)
+          let amount = (data_required.amount)
+          let date = (data_required.date)
+          let desc = data_required.description
+          let uid = data_required.uid
+
+          
 
           var cell0 = row.insertCell(0); var cell1 = row.insertCell(1); var cell2 = row.insertCell(2);
-          var cell3 = row.insertCell(3); 
+          var cell3 = row.insertCell(3); var cell4 = row.insertCell(4);
 
           cell0.innerHTML = start_sn; cell1.innerHTML = title; cell2.innerHTML = amount; cell3.innerHTML = date;
+
+          var bu = document.createElement("button")
+          bu.className = "bwt"
+          bu.id = String(uid)
+          bu.title = String(title)
+          bu.innerHTML ="Resolve"
+          bu.onclick =  () =>{
+            resolvePayment(user,title, amount, desc, date, uid)
+          }
+          cell4.appendChild(bu) 
 
           start_sn += 1
         });
       }
-      
+
+      async function resolvePayment(user,title, amount, desc, date, uid) {
+        console.log(user,title, amount, desc, date, uid);   
+        try{
+            const docRef = await setDoc(doc(db, String(user), "Transactions", "Income", uid),{
+            type: "Income" , title : title, category : "-", amount: amount, date : date, description : desc, uid: uid,
+            })
+            const docRefDel = await deleteDoc(doc(db, String(user), "Transactions", "Owed Payments", uid))
+            console.log(docRef)
+            console.log(docRefDel)
+            }
+          catch(error) {
+              console.error("Error adding document: ", error);
+          }
+        display(user)
+        }
+
       display(this.user)
       
     } 
   },
+
+
 }
 
 </script>
